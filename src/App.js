eabfb7a31241e77a4724655af999a98631682637
Sergey Sivchenko <sergey.sivchenko@effective-soft.com>
2022-11-04 14:23:14 +0300

    feat(week-3): add first todo list static page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Main from "./pages/Main";
 
-function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>This is my first react app...</h1>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <Main />;
+};
 
 export default App;
---
+++ src/pages/Main/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #282c34;
+  color: #fafafa;
+  height: 100vh;
+  padding: 16px 32px;
+`;
