df1c7915680c6d42587e83f7baa2ce481df60cdd
Sergey Sivchenko <sergey.sivchenko@effective-soft.com>
2022-11-28 14:22:42 +0300

    test(components): add IconButton tests

---
+++ src/components/IconButton/__tests__/index.js
@@ -0,0 +1,25 @@
+import { render, screen } from "utils/testing";
+
+import IconButton from "..";
+
+describe("<IconButton/>", () => {
+  it("matches snapshot with required props only", () => {
+    const onClick = jest.fn();
+    const Icon = jest.fn(({ size, title }) => `Icon: size - ${size}, title - ${title}`);
+
+    const { asFragment } = render(<IconButton icon={Icon} onClick={onClick} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("calls onClick by a component click", async () => {
+    const onClick = jest.fn();
+    const Icon = jest.fn(({ size, title }) => `Icon: size - ${size}, title - ${title}`);
+
+    const { user } = render(<IconButton icon={Icon} onClick={onClick} />);
+
+    await user.click(screen.getByRole("button"));
+
+    expect(onClick).toBeCalledTimes(1);
+  });
+});
